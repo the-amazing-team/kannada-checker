d831bd7bdaf0600d56226e32e5fe34790f8da7cc
dusklight00 <r.rahul.developer@gmail.com>
2024-01-14 14:10:37 +0530

    progress bar completed

--- main.py
+++ main.py
@@ -66,6 +66,25 @@ def draw_boxes(image, results):
     return image
 
 
+def add_kannada_progress_bar(image, percentage):
+    # shift the image down by 50 pixels
+    image = cv2.copyMakeBorder(image, 50, 0, 0, 0, cv2.BORDER_CONSTANT)
+
+    height, width, _ = image.shape
+    cv2.rectangle(image, (0, 0), (width, 50), (0, 0, 0), -1)
+    cv2.rectangle(image, (0, 0), (int(width * percentage / 100), 50), (0, 255, 0), -1)
+    cv2.putText(
+        image,
+        "Progress : " + str(round(percentage, 2)) + "%",
+        (10, 30),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (255, 255, 255),
+        2,
+    )
+    return image
+
+
 reader = easyocr.Reader(["en", "kn"])
 translator = Translator(from_lang="kn-IN", to_lang="en")
 
@@ -83,6 +102,8 @@ for result in results:
     if language != "en":
         text = translator.translate(text)
     image = draw_box(image, rect, text, confidence, language)
+
+image = add_kannada_progress_bar(image, 70)
 
 cv2.imshow("Image", image)
 cv2.waitKey(0)
